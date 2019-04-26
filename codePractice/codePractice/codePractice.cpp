a9f11cef8df7be53315b79b39df225cb9eafd504
ppeng <pengzhen1995@outlook.com>
2019-04-26 10:44:11 +0800

    虚函数使用

---
+++ codePractice/codePractice/codePractice.cpp
@@ -0,0 +1,30 @@
+// codePractice.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+//虚函数使用 链接：https://www.nowcoder.com/questionTerminal/3cea911c23a043ae96f0767d42bfa4f2
+
+
+class Base {
+public:
+	Base() { echo(); }
+	virtual void echo() { printf("Base"); }
+};
+
+class Derived :public Base {
+public:
+	Derived() { echo(); }
+	virtual void echo() { printf("Derived"); }
+};
+
+int main() {
+	Base* base = new Derived();
+	base->echo();
+	return 0;
+}
+
+
