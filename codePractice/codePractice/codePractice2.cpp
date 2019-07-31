044e41a4d1130fe3ff3a3bf02c13c9b2a8edb5e1
ppeng <pengzhen1995@outlook.com>
2019-07-31 14:59:00 +0800

    剑指6 从尾到头打印链表

--- codePractice/codePractice/codePractice2.cpp
+++ codePractice/codePractice/codePractice2.cpp
@@ -1884,15 +1884,161 @@ void merge(int arr1[], int arr2[], int length1, int length2){
 
 }
 
-int main()
+//int main()
+//{
+//	int arry2[] = { 1, 2, 3, 4 };
+//	int arry1[8] = { 5, 6, 7, 8 };
+//	int length1 = 4;
+//	int length2 = 4;
+//	merge(arry1, arry2, 4, 4);
+//	for (int i = 0; i <8; i++)
+//	{
+//		cout << arry1[i] << ',' << endl;
+//	}
+//}
+
+
+//剑指6 从尾到头打印链表
+
+struct ListNode{
+	int m_nValue;
+	ListNode* m_pNext;
+};
+
+void PrintList(ListNode* pHead)
 {
-	int arry2[] = { 1, 2, 3, 4 };
-	int arry1[8] = { 5, 6, 7, 8 };
-	int length1 = 4;
-	int length2 = 4;
-	merge(arry1, arry2, 4, 4);
-	for (int i = 0; i <8; i++)
+	printf("PrintList starts.\n");
+
+	ListNode* pNode = pHead;
+	while (pNode != nullptr)
 	{
-		cout << arry1[i] << ',' << endl;
+		printf("%d\t", pNode->m_nValue);
+		pNode = pNode->m_pNext;
 	}
+
+	printf("\nPrintList ends.\n");
+}
+ListNode* CreateListNode(int value)
+{
+	ListNode* pNode = new ListNode();
+	pNode->m_nValue = value;
+	pNode->m_pNext = nullptr;
+
+	return pNode;
+}
+
+void ConnectListNodes(ListNode* pCurrent, ListNode* pNext)
+{
+	if (pCurrent == nullptr)
+	{
+		printf("Error to connect two nodes.\n");
+		exit(1);
+	}
+
+	pCurrent->m_pNext = pNext;
+}
+
+void DestroyList(ListNode* pHead)
+{
+	ListNode* pNode = pHead;
+	while (pNode != nullptr)
+	{
+		pHead = pHead->m_pNext;
+		delete pNode;
+		pNode = pHead;
+	}
+}
+
+//核心代码 
+//方法一 栈
+void PrintListReversingly_Iteratively(ListNode* pHead)
+{
+	std::stack<ListNode*> nodes;
+
+	ListNode* pNode = pHead;
+	while (pNode != nullptr)
+	{
+		nodes.push(pNode);
+		pNode = pNode->m_pNext;
+	}
+
+	while (!nodes.empty())
+	{
+		pNode = nodes.top();
+		printf("%d\t", pNode->m_nValue);
+		nodes.pop();
+	}
+}
+
+//方法二：递归
+void PrintListReversingly_Recursively(ListNode* pHead)
+{
+	if (pHead != nullptr)
+	{
+		if (pHead->m_pNext != nullptr)
+		{
+			PrintListReversingly_Recursively(pHead->m_pNext);
+		}
+
+		printf("%d\t", pHead->m_nValue);
+	}
+}
+
+// ====================测试代码====================
+void Test(ListNode* pHead)
+{
+	PrintList(pHead);
+	PrintListReversingly_Iteratively(pHead);
+	printf("\n");
+	PrintListReversingly_Recursively(pHead);
+}
+
+// 1->2->3->4->5
+void Test61()
+{
+	printf("\nTest1 begins.\n");
+
+	ListNode* pNode1 = CreateListNode(1);
+	ListNode* pNode2 = CreateListNode(2);
+	ListNode* pNode3 = CreateListNode(3);
+	ListNode* pNode4 = CreateListNode(4);
+	ListNode* pNode5 = CreateListNode(5);
+
+	ConnectListNodes(pNode1, pNode2);
+	ConnectListNodes(pNode2, pNode3);
+	ConnectListNodes(pNode3, pNode4);
+	ConnectListNodes(pNode4, pNode5);
+
+	Test(pNode1);
+
+	DestroyList(pNode1);
+}
+
+// 只有一个结点的链表: 1
+void Test62()
+{
+	printf("\nTest2 begins.\n");
+
+	ListNode* pNode1 = CreateListNode(1);
+
+	Test(pNode1);
+
+	DestroyList(pNode1);
+}
+
+// 空链表
+void Test63()
+{
+	printf("\nTest3 begins.\n");
+
+	Test(nullptr);
+}
+
+int main(int argc, char* argv[])
+{
+	Test61();
+	Test62();
+	Test63();
+
+	return 0;
 }
